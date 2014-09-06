697edebcbe315d05170fe228a8a042e1e0c3bb03
scott-zgeng <scott.zgeng@gmail.com>
2014-09-07 01:36:33 +0800

    hash_group_5

--- src/kernel/hash_group_node.cpp
+++ src/kernel/hash_group_node.cpp
@@ -51,6 +51,7 @@ result_t row_segement_t::add_column(expr_base_t* expr)
     item.expr = expr;
     item.offset = m_row_len;
     item.size = calc_data_len(expr->data_type());
+    
     m_row_len += item.size;
 
     bool is_succ = m_columns.push_back(item);
@@ -87,6 +88,115 @@ result_t row_segement_t::next(rowset_t* rows, mem_stack_t* mem, mem_handle_t res
 
 
 
+template<typename T>
+struct group_op_t : public group_op_base_t
+{
+    virtual db_uint32 calc_hash(db_uint32 hash, void* row) {
+        unsigned int seed = 131;        
+        char* input = (char*)row;
+
+        for (size_t i = 0; i < sizeof(T); i++) {
+            hash = hash * seed + input[i];
+        }
+        return hash;
+    }
+
+    virtual db_bool is_equal(void* left_row, void* right_row) {
+        return (*(T*)left_row == *(T*)right_row); 
+    }
+};
+
+
+template<>
+struct group_op_t<db_string>: public group_op_base_t
+{
+    virtual db_uint32 calc_hash(db_uint32 hash, void* row) {
+        unsigned int seed = 131;
+        char* input = (char*)row;
+
+        while (*input != 0) {
+            hash = hash * seed + *input;
+            input++;
+        }
+        return hash;
+    }
+
+    virtual db_bool is_equal(void* left_row, void* right_row) {
+        return (strcmp((db_char*)left_row, (db_char*)right_row) == 0);        
+    }
+};
+
+
+
+group_op_base_t* aggr_table_t::create_group_op(data_type_t type)
+{
+    switch (type)
+    {    
+    case DB_INT8:
+        return new group_op_t<db_int8>();
+    case DB_INT16:
+        return new group_op_t<db_int16>();
+    case DB_INT32:
+        return new group_op_t<db_int32>();
+    case DB_INT64:
+        return new group_op_t<db_int64>();
+    case DB_FLOAT:
+        return new group_op_t<db_float>();
+    case DB_DOUBLE:
+        return new group_op_t<db_double>();
+    case DB_STRING:
+        return new group_op_t<db_string>();
+    default:
+        return NULL;
+    }
+}
+
+
+template<typename T, aggr_type_t AT>
+struct aggr_op_t : public aggr_op_base_t 
+{
+    virtual void update(void* dst_row, void* src_row) {}
+};
+
+
+template<>
+class aggr_op_t<db_int64, AGGR_FUNC_COUNT> : public aggr_op_base_t
+{
+    virtual void update(void* dst_row, void* src_row) {
+        *(db_int64*)dst_row += *(db_int64*)src_row;
+    }
+};
+
+
+//
+//template<aggr_type_t AT>
+//aggr_op_base_t* aggr_table_t::create_aggr_op_impl(data_type_t type)
+//{
+//
+//}
+
+aggr_op_base_t* aggr_table_t::create_aggr_op(data_type_t type, aggr_type_t aggr_type)
+{
+    switch (aggr_type)
+    {    
+    case AGGR_COLUMN:
+        break;
+    case AGGR_FUNC_COUNT:
+        return new aggr_op_t<db_int64, AGGR_FUNC_COUNT>();
+    case AGGR_FUNC_SUM:
+        break;
+    case AGGR_FUNC_AVG:
+        break;
+    case AGGR_FUNC_MIN:
+        break;
+    case AGGR_FUNC_MAX:
+        break;
+    default:
+        break;
+    }
+
+    return NULL;
+}
 
 
 
@@ -116,13 +226,12 @@ result_t hash_group_node_t::init(Parse* parse, Select* select)
     }
 
     for (db_int32 i = 0; i < select->pGroupBy->nExpr; i++) {
-        group_item_t item;
-        item.values = NULL;
-        ret = factory.build(select->pGroupBy->a[i].pExpr, &item.expr);
+        expr_base_t* expr = NULL;
+        ret = factory.build(select->pGroupBy->a[i].pExpr, &expr);
         IF_RETURN_FAILED(ret != RT_SUCCEEDED);
 
-        bool is_succ = m_group_columns.push_back(item);
-        IF_RETURN_FAILED(!is_succ);
+        ret = m_aggr_table.add_group_column(expr);
+        IF_RETURN_FAILED(ret != RT_SUCCEEDED);
     }
 
     ret = m_rowset.init(m_children->rowid_size());
@@ -141,32 +250,32 @@ result_t hash_group_node_t::add_aggr_sub_expr(expr_factory_t& factory, Expr* pEx
     if (pExpr->op == TK_AGG_FUNCTION) {
         assert(pExpr->x.pList == NULL || pExpr->x.pList->nExpr == 1);
 
-        aggr_item_t item = {NULL, AGGR_UNKNOWN, NULL};        
+        expr_base_t* expr = NULL;
         
         if (pExpr->x.pList != NULL) {
-            ret = factory.build(pExpr->x.pList->a->pExpr, &item.expr);
+            ret = factory.build(pExpr->x.pList->a->pExpr, &expr);
             IF_RETURN_FAILED(ret != RT_SUCCEEDED);
         }
 
-        item.type = get_aggr_type(pExpr->u.zToken);
-        IF_RETURN_FAILED(item.type == AGGR_UNKNOWN);
+        aggr_type_t aggr_type = get_aggr_type(pExpr->u.zToken);
+        IF_RETURN_FAILED(aggr_type == AGGR_UNKNOWN);
 
-        bool is_succ = m_aggr_columns.push_back(item);
-        IF_RETURN_FAILED(!is_succ);
+        ret = m_aggr_table.add_aggr_column(expr, aggr_type);
+        IF_RETURN_FAILED(ret != RT_SUCCEEDED);
 
         return RT_SUCCEEDED;
 
     } else if (pExpr->op == TK_AGG_COLUMN) {        
         Expr clone_expr;
         memcpy(&clone_expr, pExpr, sizeof(clone_expr));
-        clone_expr.op = TK_COLUMN;        
-        aggr_item_t item = { NULL, AGGR_COLUMN, NULL };
+        clone_expr.op = TK_COLUMN;                
 
-        ret = factory.build(&clone_expr, &item.expr);
+        expr_base_t* expr = NULL;
+        ret = factory.build(&clone_expr, &expr);
         IF_RETURN_FAILED(ret != RT_SUCCEEDED);
 
-        bool is_succ = m_aggr_columns.push_back(item);
-        IF_RETURN_FAILED(!is_succ);
+        ret = m_aggr_table.add_aggr_column(expr, AGGR_COLUMN);
+        IF_RETURN_FAILED(ret != RT_SUCCEEDED);
 
         return RT_SUCCEEDED;
 
@@ -178,7 +287,6 @@ result_t hash_group_node_t::add_aggr_sub_expr(expr_factory_t& factory, Expr* pEx
         IF_RETURN_FAILED(ret != RT_SUCCEEDED);
     }
 
-
     if (pExpr->pRight != NULL) {
         ret = add_aggr_sub_expr(factory, pExpr->pRight);
         IF_RETURN_FAILED(ret != RT_SUCCEEDED);
@@ -189,7 +297,7 @@ result_t hash_group_node_t::add_aggr_sub_expr(expr_factory_t& factory, Expr* pEx
 
 
 
-hash_group_node_t::aggr_type_t hash_group_node_t::get_aggr_type(const char* token)
+aggr_type_t hash_group_node_t::get_aggr_type(const char* token)
 {
     assert(token != NULL);
 
@@ -224,27 +332,8 @@ result_t hash_group_node_t::build(mem_stack_t* mem)
 
         if (m_rowset.count() == 0) break;
 
-        mem_handle_t spin_handle;
-        mem->spin_memory(spin_handle);
-
-        for (db_uint32 i = 0; i < m_group_columns.size(); i++) {
-            mem_handle_t handle;
-            group_item_t& item = m_group_columns[i];
-            ret = item.expr->calc(&m_rowset, mem, handle);
-            IF_RETURN_FAILED(ret != RT_SUCCEEDED); 
-
-            item.values = handle.transfer();            
-        }
-
-        for (db_uint32 i = 0; i < m_aggr_columns.size(); i++) {
-            mem_handle_t handle;
-            aggr_item_t& item = m_aggr_columns[i];
-            ret = item.expr->calc(&m_rowset, mem, handle);
-            IF_RETURN_FAILED(ret != RT_SUCCEEDED);
-
-            item.values = handle.transfer();
-        }
-
+        ret = m_aggr_table.next(&m_rowset, mem);
+        IF_RETURN_FAILED(ret != RT_SUCCEEDED);
 
         if (m_rowset.count() < SEGMENT_SIZE) break;
     }
@@ -261,8 +350,6 @@ result_t hash_group_node_t::next(rowset_t* rows, mem_stack_t* mem)
         m_first = false;
     }
         
-
-
     //m_aggr_table.
     return RT_FAILED;
 }
--- src/kernel/hash_group_node.h
+++ src/kernel/hash_group_node.h
@@ -19,12 +19,16 @@ public:
 public:
     result_t add_column(expr_base_t* expr);
     result_t next(rowset_t* rows, mem_stack_t* mem, mem_handle_t result);
-    
-protected:
+
+    db_uint32 row_len() const {
+        return m_row_len;
+    }
+
+private:
     struct expr_item_t {
         expr_base_t* expr;
         db_uint32 offset;
-        db_uint32 size;
+        db_uint32 size;        
     };
 
     db_uint32 m_row_len;
@@ -32,78 +36,196 @@ protected:
 };
 
 
+enum aggr_type_t {
+    AGGR_UNKNOWN = 0,
+    AGGR_COLUMN = 1,
+    AGGR_FUNC_COUNT = 2,
+    AGGR_FUNC_SUM = 3,
+    AGGR_FUNC_AVG = 4,
+    AGGR_FUNC_MIN = 5,
+    AGGR_FUNC_MAX = 6,
+};
 
-
-
-class group_columns_t
+struct group_op_base_t
 {
-public:
-    result_t init(database_t* db);
-    result_t add_column(expr_base_t* expr);
-    result_t next(rowset_t* rows, mem_stack_t* mem);
+    group_op_base_t() { offset = 0; }
+    virtual ~group_op_base_t() {}
+    virtual db_uint32 calc_hash(db_uint32 hash_val, void* row) = 0;
+    virtual db_bool is_equal(void* left_row, void* right_row) = 0;
+
+    db_uint32 offset;
+};
 
 
-private:
-    row_segement_t m_rows;
+struct aggr_op_base_t
+{
+    virtual ~aggr_op_base_t() {}
+    virtual void update(void* dst_row, void* src_row) = 0;
+
+    db_uint32 offset;
 };
 
 
 
-// init()
-// insert();
+class aggr_table_t
+{
+private:
+    struct hash_node_t {
+        db_uint32 hash_val;
+        db_byte* key;
+        db_byte* value;
+        hash_node_t* next;
+    };
+    
+public:
+    aggr_table_t() {
+        m_hash_size = 0;
+        m_hash_table = NULL;
+    }
 
-// for group
-// hash()
-// compare()
+    ~aggr_table_t() {
+        if (m_hash_table != NULL) {
+            free(m_hash_table);
+            m_hash_table = NULL;
+        }        
+    }
 
-// for aggr
-// update()
+    result_t init(database_t* db, db_uint32 row_count) {
+        m_group_region.init(db->get_mem_pool());
+        m_aggr_region.init(db->get_mem_pool());
+        m_hash_region.init(db->get_mem_pool());
+
+        m_hash_size = 393241; // TODO(scott.zgeng): add cala hash size function
+        m_hash_table = (hash_node_t**)malloc(m_hash_size * sizeof(hash_node_t*));
+        IF_RETURN_FAILED(m_hash_table == NULL);        
+
+        memset(m_hash_table, 0, sizeof(hash_node_t*) * m_hash_size);
+
+        return RT_SUCCEEDED;
+    }
 
 
+    group_op_base_t* create_group_op(data_type_t type);
+    aggr_op_base_t* create_aggr_op(data_type_t type, aggr_type_t aggr_type);
 
-//
-//template<db_uint32 INIT_SIZE>
-//class nsm_columns_t
-//{
-//public:
-//    nsm_columns_t();
-//    ~nsm_columns_t();
-//
-//public:    
-//    result_t add_column(expr_base_t* expr) {
-//        expr->data_type();  
-//
-//    }
-//    
-//
-//
-//
-//};
-////
-//
-//class aggr_table_t
-//{ 
-//
-//public:
-//    aggr_table_t(database_t* db) :  {
-//        m_mem_region.init(db->get_mem_pool());
-//    }
-//
-//
-//    result_t insert_update() {
-//
-//    }
-//
-//
-//    void insert();
-//    void update();
-//
-//
-//private:
-//   
-//    mem_region_t m_mem_region;
-//};
+    result_t add_group_column(expr_base_t* expr) {
+        group_op_base_t* op = create_group_op(expr->data_type());
+        db_bool is_succ = m_group_ops.push_back(op);
+        IF_RETURN_FAILED(!is_succ);        
+        op->offset = m_group_rows.row_len();
 
+        return m_group_rows.add_column(expr);
+    }
+
+    result_t add_aggr_column(expr_base_t* expr, aggr_type_t aggr_type) {
+        aggr_op_base_t* op = create_aggr_op(expr->data_type(), aggr_type);
+        db_bool is_succ = m_aggr_ops.push_back(op);
+        IF_RETURN_FAILED(!is_succ);        
+        op->offset = m_aggr_rows.row_len();
+
+        return m_aggr_rows.add_column(expr);
+    }
+
+    result_t next(rowset_t* rows, mem_stack_t* mem) {
+        result_t ret;
+        mem_handle_t group_handle;
+        ret = m_group_rows.next(rows, mem, group_handle);
+        IF_RETURN_FAILED(ret != RT_SUCCEEDED);
+
+        mem_handle_t aggr_handle;
+        ret = m_aggr_rows.next(rows, mem, aggr_handle);
+        IF_RETURN_FAILED(ret != RT_SUCCEEDED);
+            
+        db_byte* group_row = (db_byte*)group_handle.ptr();
+        db_byte* aggr_row = (db_byte*)aggr_handle.ptr();
+
+        for (db_uint32 i = 0; i < rows->count(); i++) {
+            ret = insert_update(group_row, aggr_row);
+            IF_RETURN_FAILED(ret != RT_SUCCEEDED);
+            
+            group_row += m_group_rows.row_len();
+            aggr_row += m_aggr_rows.row_len();
+        }
+
+        return RT_SUCCEEDED;
+    }
+
+    result_t insert_update(db_byte* group_row, db_byte* aggr_row) {
+        db_uint32 hash_val = calc_hash(group_row);
+
+        hash_node_t* node = m_hash_table[hash_val % m_hash_size];
+        while (node != NULL) {
+            if (node->hash_val == hash_val && is_equal(node->key, group_row)) {
+                update(node->value, aggr_row);
+                return RT_SUCCEEDED;
+            }
+            node = node->next;
+        }
+
+        return insert(hash_val, group_row, aggr_row);
+    }
+
+    result_t insert(db_uint32 hash_val, db_byte* group_row, db_byte* aggr_row) {
+        // TODO(scott.zgeng): 目前只做了浅拷贝，后续看是否需要改为深拷贝
+
+        hash_node_t* hash_node = (hash_node_t*)m_hash_region.alloc(sizeof(hash_node_t));
+        IF_RETURN_FAILED(hash_node == NULL);
+
+        hash_node->key = (db_byte*)m_group_region.alloc(m_group_rows.row_len());
+        IF_RETURN_FAILED(hash_node->key == NULL);
+        memcpy(hash_node->key, group_row, m_group_rows.row_len());
+
+        hash_node->value = (db_byte*)m_aggr_region.alloc(m_aggr_rows.row_len());
+        IF_RETURN_FAILED(hash_node->value == NULL);
+        memcpy(hash_node->value, aggr_row, m_aggr_rows.row_len());
+
+        hash_node_t*& entry = m_hash_table[hash_val % m_hash_size];
+        hash_node->next = entry;
+        entry = hash_node;
+
+        return RT_SUCCEEDED;
+    }
+
+    void update(db_byte* dst_row, db_byte* src_row) {        
+        for (db_uint32 i = 0; i < m_aggr_ops.size(); i++) {
+            aggr_op_base_t* op = m_aggr_ops[i];
+            op->update(dst_row + op->offset, src_row + op->offset);
+        }        
+    }
+
+    db_uint32 calc_hash(db_byte* row) {        
+        db_uint32 hash_val = 0;
+        for (db_uint32 i = 0; i <m_group_ops.size(); i++) {
+            group_op_base_t* op = m_group_ops[i];
+            hash_val = op->calc_hash(hash_val, row + op->offset);
+        }
+        return hash_val;
+    }
+
+    db_bool is_equal(db_byte* left_row, db_byte* right_row) {        
+        for (db_uint32 i = 0; i < m_group_ops.size(); i++) {
+            group_op_base_t* op = m_group_ops[i];
+            if (!op->is_equal(left_row + op->offset, right_row + op->offset)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+private:
+    mem_region_t m_group_region;
+    mem_region_t m_aggr_region;
+    mem_region_t m_hash_region;
+
+    row_segement_t m_group_rows;
+    row_segement_t m_aggr_rows;
+
+    pod_vector<group_op_base_t*> m_group_ops;
+    pod_vector<aggr_op_base_t*> m_aggr_ops;
+
+    hash_node_t** m_hash_table;
+    db_uint32 m_hash_size;    
+};
 
 
 
@@ -113,15 +235,7 @@ public:
     static const db_uint32 MAX_GROUP_COLUMNS = 16;
     static const db_uint32 MAX_AGGR_COLUMNS = 16;
 public:
-    enum aggr_type_t {
-        AGGR_UNKNOWN = 0,
-        AGGR_COLUMN = 1,
-        AGGR_FUNC_COUNT = 2,        
-        AGGR_FUNC_SUM = 3,
-        AGGR_FUNC_AVG = 4,
-        AGGR_FUNC_MIN = 5,
-        AGGR_FUNC_MAX = 6,
-    };
+
 
 public:
     hash_group_node_t(database_t* db, node_base_t* children);
@@ -139,28 +253,14 @@ private:
     result_t build(mem_stack_t* mem);
 
 private:   
-    struct group_item_t {
-        expr_base_t* expr;
-        void* values;
-    };
-
-    struct aggr_item_t {
-        expr_base_t* expr;
-        aggr_type_t type;
-        void* values;
-    };
-
     node_base_t* m_children;
     database_t* m_database;
-    //aggr_table_t m_aggr_table;
 
-    pod_vector<group_item_t, MAX_GROUP_COLUMNS> m_group_columns;
-    pod_vector<aggr_item_t, MAX_AGGR_COLUMNS> m_aggr_columns;
+    aggr_table_t m_aggr_table;
     
     db_bool m_first;
     rowset_t m_rowset;
 };
-
 
 
 #endif //__HASH_GROUP_NODE_H__
